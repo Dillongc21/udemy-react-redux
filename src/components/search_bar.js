989ff73039daa355c4646bc738e358294171eb76
dillonchristensen <Dillongc21@gmail.com>
2017-10-28 14:26:12 -0600

    Intro to State

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react'
 
 class SearchBar extends Component {
   render() {
+    constructor(props) {
+      super(props);
+
+      this.state = { term : '' }
+    }
+
     return <input onChange={event => console.log(event.target.value)} />;
   }
 }
