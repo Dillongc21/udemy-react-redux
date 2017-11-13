e0819f51a2368f0d505dbb506df8983c34d4767b
dillonchristensen <Dillongc21@gmail.com>
2017-11-13 01:20:47 -0700

    YouTube Search Response

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from "youtube-api-search";
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAZ1GP8Nl-gvug3Gq6nraQM0jc6dJ8F9OU';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 // Create a new component. This component should produce
 // some HTML
